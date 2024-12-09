cd89c42c5ba511e5422eb05081469d037a010a9d
Fowmi <96404660+Fowmi@users.noreply.github.com>
2024-12-09 12:24:49 +0530

    Volume change to gp3

---
+++ Lambda/lambda_function.py
@@ -0,0 +1,35 @@
+import boto3
+def get_volume_id_from_arn(volume_arn):
+    # Ensure the ARN is valid and contains "volume/"
+       return volume_arn.split(":")[5].split("/")[1] if "volume/" in volume_arn and len(volume_arn.split(":")) > 5 else ""
+
+def lambda_handler(event, context):
+    # Extract the volume ARN from the event
+    volume_arn = event['resources'][0]
+    # Get the volume ID from the ARN
+    volume_id = get_volume_id_from_arn(volume_arn)
+    
+    if not volume_id:
+        return {
+            "statusCode": 400,
+            "message": "Invalid ARN or volume ID not found."
+        }
+    
+    # Initialize EC2 client
+    ec2_client = boto3.client('ec2')
+    try:
+        # Modify the volume
+        response = ec2_client.modify_volume(
+            VolumeId=volume_id,
+            VolumeType="gp3"
+        )
+        return {
+            "statusCode": 200,
+            "message": "Volume modified successfully",
+            "response": response
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "message": f"Failed to modify volume: {str(e)}"
+        }
